fe38ee5b68c96091a90ae4ef224627cfd409d944
Wes Peacock <wes_peacock@sil.org>
2023-12-17 14:57:50 -0700

    Add processing for correct options/ini fileitems
     * mostly copied from Adjusthm.pl

--- FlagXtXref.pl
+++ FlagXtXref.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/env perl
-my $USAGE = "Usage: $0 [--inifile inifile.ini] [--section FlagXtXref] [--eolrep #] [--reptag __hash__] [--debug] [file.sfm]\nA script that flags cross references for existence, ambiguity and duplication";
+my $USAGE = "Usage: $0 [--inifile inifile.ini] [--section FlagXtXref] [--logfile logfile.log] [--errfile errfile.err] [--eolrep #] [--reptag __hash__] [--debug] [file.sfm]\nA script that flags extended cross references for existence, ambiguity and duplication";
 
 =pod
-This script is a stub that provides the code for opl'ing and de_opl'ing an input file
-It also includes code to:
-	- use an ini file (commented out)
-	- process command line options including debugging
+This script checks extended cross references.
+An extended cross reference is one where the target has more than just the lx/lc field of the target.
+It flags them as to the existence and status of the target:
+Missing in the lc/lx/se fields
+Ambiguous (Found but could be one of multiple homographs)
+Missing in lc/lx/se but
+found in another cf/lv – need to resolve.
+not found
+Found in a single – need to resolve/add to the other entry.
+
 
 The ini file should have sections with syntax like this:
 [FlagXtXref]
@@ -19,6 +25,7 @@ dtmarks=dt,date
 preprocess=s{^\\lf .*?__LS__}{};
 
 =cut
+
 use 5.020;
 use utf8;
 use open qw/:std :utf8/;
@@ -28,40 +35,76 @@ use warnings;
 use English;
 use Data::Dumper qw(Dumper);
 
-
 use File::Basename;
 my $scriptname = fileparse($0, qr/\.[^.]*/); # script name without the .pl
+$USAGE =~ s/inifile\./$scriptname\./;
+$USAGE =~ s/errfile\.err/$scriptname\-err.txt/;
+$USAGE =~ s/logfile\.log/$scriptname\-log.txt/;
 
 use Getopt::Long;
 GetOptions (
 	'inifile:s'   => \(my $inifilename = "$scriptname.ini"), # ini filename
-	'section:s'   => \(my $inisection = "section"), # section of ini file to use
+	'section:s'   => \(my $inisection = "FlagXtXref"), # section of ini file to use
+	'logfile:s'   => \(my $logfilename = "$scriptname-log.txt"), # log filename
+	'errfile:s'   => \(my $errfilename = "$scriptname-err.txt"), # Error filename
+	'eolrep:s' => \(my $eolrep = "#"), # character used to replace EOL
+	'reptag:s' => \(my $reptag = "__hash__"), # tag to use in place of the EOL replacement character
+
+	'help'    => \my $help,
 # additional options go here.
 # 'sampleoption:s' => \(my $sampleoption = "optiondefault"),
 	'recmark:s' => \(my $recmark = "lx"), # record marker, default lx
-	'eolrep:s' => \(my $eolrep = "#"), # character used to replace EOL
-	'reptag:s' => \(my $reptag = "__hash__"), # tag to use in place of the EOL replacement character
-	# e.g., an alternative is --eolrep % --reptag __percent__
-
-	# Be aware # is the bash comment character, so quote it if you want to specify it.
-	#	Better yet, just don't specify it -- it's the default.
 	'debug'       => \my $debug,
 	) or die $USAGE;
+if ($help) {
+	say STDERR $USAGE;
+	exit;
+	}
 
-# check your options and assign their information to variables here
-$recmark =~ s/[\\ ]//g; # no backslashes or spaces in record marker
 
-# if you do not need a config file ucomment the following and modify it for the initialization you need.
-# if you have set the $inifilename & $inisection in the options, you only need to set the parameter variables according to the parameter names
-# =pod
+open(my $ERRFILE, '>', $errfilename)
+	or die "Could not open Error file '$errfilename' $!";
+
+open(my $LOGFILE, '>', $logfilename)
+		or die "Could not open Log file '$logfilename' $!";
+
+say STDERR "inisection:$inisection" if $debug;
+
 use Config::Tiny;
 my $config = Config::Tiny->read($inifilename, 'crlf');
-die "Quitting: couldn't find the INI file $inifilename\n$USAGE\n" if !$config;
-my $param1 = $config->{"$inisection"}->{Param1};
-say STDERR "Param1:$param1" if $debug;
-my $param2 = $config->{"$inisection"}->{Param2};
-say STDERR "Param2:$param2" if $debug;
-# =cut
+
+my $hmmark;
+my $srchSEmarks;
+my $srchVAmarks;
+my $lcmark;
+if ($config) {
+	$recmark = $config->{"$inisection"}->{recmark};
+	$hmmark = $config->{"$inisection"}->{hmmark};
+	$lcmark = $config->{"$inisection"}->{lcmark};
+	my $semarks = $config->{"$inisection"}->{semarks};
+	my $vamarks = $config->{"$inisection"}->{vamarks};
+	$vamarks = "\N{INVERTED QUESTION MARK}\N{INVERTED QUESTION MARK}" if ! $vamarks; # should never match
+	for ($recmark, $hmmark, $lcmark, $semarks,$vamarks) {
+		# remove backslashes and spaces from the SFMs in the INI file
+		say STDERR $_ if $debug;
+		s/\\//g;
+		s/ //g;
+		}
+	for ($semarks, $vamarks) {
+		s/\,*$//; # no trailing commas
+		s/\,/\|/g;  # use bars for or'ing
+		}
+	$srchSEmarks = qr/$semarks/;
+	$srchVAmarks = qr/$vamarks/;
+	}
+else {
+	die  "Couldn't find the INI file: $inifilename\n";
+	}
+say STDERR "record mark:$recmark" if $debug;
+say STDERR "homograph mark:$hmmark" if $debug;
+say STDERR "subentry marks Match: $srchSEmarks" if $debug;
+say STDERR "variant marks Match: $srchVAmarks" if $debug;
+say STDERR "citation mark:$lcmark" if $debug;
 
 # generate array of the input file with one SFM record per line (opl)
 my @opledfile_in;
